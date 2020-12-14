3f7dbef58b277a4eca5b04a21aef2f4df3da0d39
joe-bookwood <github@buchholz-it.de>
2020-12-14 09:39:26 +0100

    extended logging

--- src/main/java/de/bitc/jackson/IsEmptyDeserializationWrapper.java
+++ src/main/java/de/bitc/jackson/IsEmptyDeserializationWrapper.java
@@ -47,19 +47,75 @@ public class IsEmptyDeserializationWrapper extends BeanDeserializer {
 
 	}
 
-	private Object isEmptyVanillaDeserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-		p.nextToken();
+	/**
+	 * General version used when handling needs more advanced features.
+	 */
+	@Override
+	public Object deserializeFromObject(JsonParser p, DeserializationContext ctxt) throws IOException {
+		/*
+		 * 09-Dec-2014, tatu: As per [databind#622], we need to allow Object Id
+		 * references to come in as JSON Objects as well; but for now assume they will
+		 * be simple, single-property references, which means that we can recognize them
+		 * without having to buffer anything. Once again, if we must, we can do more
+		 * complex handling with buffering, but let's only do that if and when that
+		 * becomes necessary.
+		 */
+		if ((_objectIdReader != null) && _objectIdReader.maySerializeAsObject()) {
+			if (p.hasTokenId(JsonTokenId.ID_FIELD_NAME)
+					&& _objectIdReader.isValidReferencePropertyName(p.getCurrentName(), p)) {
+				logger.debug("deserializeFromObjectId {}", p.getCurrentName());
+				return deserializeFromObjectId(p, ctxt);
+			}
+		}
+		if (_nonStandardCreation) {
+			if (_unwrappedPropertyHandler != null) {
+				logger.debug("deserializeWithUnwrapped");
+				return deserializeWithUnwrapped(p, ctxt);
+			}
+			if (_externalTypeIdHandler != null) {
+				logger.debug("deserializeWithExternalTypeId");
+				return deserializeWithExternalTypeId(p, ctxt);
+			}
+			Object bean = deserializeFromObjectUsingNonDefault(p, ctxt);
+			/*
+			 * 27-May-2014, tatu: I don't think view processing would work at this point, so
+			 * commenting it out; but leaving in place just in case I forgot something
+			 * fundamental...
+			 */
+			/*
+			 * if (_needViewProcesing) { Class<?> view = ctxt.getActiveView(); if (view !=
+			 * null) { return deserializeWithView(p, ctxt, bean, view); } }
+			 */
+			logger.debug("return bean nonStandardCreation {}", bean.getClass().getSimpleName());
+			return bean;
+		}
+		final Object bean = _valueInstantiator.createUsingDefault(ctxt);
+		// [databind#631]: Assign current value, to be accessible by custom
+		// deserializers
+		p.setCurrentValue(bean);
+		if (p.canReadObjectId()) {
+			Object id = p.getObjectId();
+			if (id != null) {
+				_handleTypedObjectId(p, ctxt, bean, id);
+			}
+		}
+		if (_injectables != null) {
+			injectValues(ctxt, bean);
+		}
+		if (_needViewProcesing) {
+			Class<?> view = ctxt.getActiveView();
+			if (view != null) {
+				logger.debug("deserializeWithView");
+				return deserializeWithView(p, ctxt, bean, view);
+			}
+		}
 		if (p.hasTokenId(JsonTokenId.ID_FIELD_NAME)) {
-			final Object bean = _valueInstantiator.createUsingDefault(ctxt);
-			// [databind#631]: Assign current value, to be accessible by custom serializers
-			p.setCurrentValue(bean);
 			String propName = p.getCurrentName();
 			do {
 				p.nextToken();
 				SettableBeanProperty prop = _beanProperties.find(propName);
-
 				if (prop != null) { // normal case
-					logger.debug("property: {}", prop.getName());
+					logger.debug("prop != null name={}", prop.getName());
 					try {
 						prop.deserializeAndSet(p, ctxt, bean);
 					} catch (Exception e) {
@@ -67,11 +123,11 @@ public class IsEmptyDeserializationWrapper extends BeanDeserializer {
 					}
 					continue;
 				}
+				logger.debug("handleUnknownVanilla");
 				handleUnknownVanilla(p, ctxt, bean, propName);
 			} while ((propName = p.nextFieldName()) != null);
-			return bean;
 		}
-		return null;
+		logger.debug("return bean {}", bean.getClass().getSimpleName());
+		return bean;
 	}
-
 }
